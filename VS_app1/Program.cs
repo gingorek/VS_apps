3672c5690e80e074bee201c596b2ea86398ffe7f
ignacylozinski <ignacy.lozinski@gmail.com>
2018-10-07 12:49:37 +0200

    this is new commit

--- VS_app1/Program.cs
+++ VS_app1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace VS_app1
 {
@@ -10,6 +6,8 @@ namespace VS_app1
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("this is sparta");
         }
     }
 }
